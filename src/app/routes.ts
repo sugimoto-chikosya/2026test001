0c4b0e6ebe7a157c6add9cc643ee8e09586cfcd2
杉本幸世 <sugimoto@wz-art.co.jp>
2026-03-31 11:22:17 +0900

    fix login import

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,8 +6,16 @@ import { CustomerAnalysis } from "./pages/CustomerAnalysis";
 import { CashFlow } from "./pages/CashFlow";
 import { ManagementReport } from "./pages/ManagementReport";
 import { Settings } from "./pages/Settings";
+import { Login } from "./pages/Login"
 
 export const router = createBrowserRouter([
+  // 👇 ログイン（Layoutなし）
+  {
+    path: "/login",
+    Component: Login,
+  },
+
+  // 👇 アプリ本体（Layoutあり）
   {
     path: "/",
     Component: Layout,
